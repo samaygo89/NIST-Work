531e1602da99a4dc60ee44294a88d2c44ac9c7c2
samaygo89 <75504344+samaygo89@users.noreply.github.com>
2021-07-20 15:19:30 -0400

    Camera Rotation
    This commit adds the ability to rotate the robot.

---
+++ RobotInterface/Assets/Scripts/Playback/RotateAroundTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundTest : MonoBehaviour
+{
+ GameObject ur5;
+public void test(){
+        ur5 = GameObject.Find("ur5");
+        ur5.transform.RotateAroundLocal(new Vector3(0,1,0),50);
+    }
+
+
+    
+        
+    
+}
