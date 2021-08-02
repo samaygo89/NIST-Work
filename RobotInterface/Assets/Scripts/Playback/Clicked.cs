4c989e245618f6eac86db628b27a80cb1e3e29cc
samaygo89 <75504344+samaygo89@users.noreply.github.com>
2021-08-02 09:32:47 -0400

    Final Changes
    Added Comments to all code

--- RobotInterface/Assets/Scripts/Playback/Clicked.cs
+++ RobotInterface/Assets/Scripts/Playback/Clicked.cs
@@ -5,49 +5,39 @@ using UnityEngine.UI;
 
 public class Clicked : MonoBehaviour
 {
-    private bool isInteger(float num)
-    {
-        if(num == (int)num && num>=1)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
     public List<Vector3> positions = new List<Vector3>();
     public GameObject target;
-
     Dropdown dropdown;
     public int count = 0;
     public int size = 0;
     public bool isPressed = false;
     public bool playOnRobot = false;
-    
     void Start()
     {
+        //Get the dropdown and target objects
         dropdown = GameObject.Find("Dropdown").GetComponent<UnityEngine.UI.Dropdown>();
         target = GameObject.Find("Target");
     }
 
-    public void FixedUpdate()
+    public void FixedUpdate() // each frame 
     {
-        count++;
+        //Increment counter
+        count++; 
+        //First check if the button was pressed
         if (isPressed)
         {
-
-
             int index;
-            if (count%150 == 0)
+            //if we are at a multiple of 150 frames(3 seconds)
+            if (count % 150 == 0)
             {
-                index = count/150;
-                if(index<=size){
-                target.transform.position = positions[index-1];
-                dropdown.value = index-1;
+                index = count / 150;
+                if (index <= size)
+                {
+                    //Show correct target and change dropdown value
+                    target.transform.position = positions[index - 1];
+                    dropdown.value = index - 1;
                 }
-                
+
 
                 //  Additionally, play on real robot
                 //  NOTE:  This is too fast because the robot command is non-blocking....
@@ -57,21 +47,24 @@ public class Clicked : MonoBehaviour
                     print("Setting pose on physical robot...");
                     GameObject.Find("Cmd2").GetComponent<Button>().onClick.Invoke();
                 }*/
-                if(index>size){
-                isPressed = false;
-                print("Playback Complete");
+                //If index is greater than size we have reached the end
+                if (index > size)
+                {
+                    isPressed = false;
+                    print("Playback Complete");
                 }
             }
-            
 
-            
+
+
         }
-        
+
     }
 
 
     public void Pressed()
     {
+        //Get the positions array
         positions = target.GetComponent<Mouse_drag>().positions;
         // New - robot playback toggle
         playOnRobot = GameObject.Find("RobotPlayback").GetComponent<Toggle>().isOn;
@@ -82,9 +75,12 @@ public class Clicked : MonoBehaviour
             print("Not Enough Points");
             return;
         }
-
+        else{
         isPressed = true;
         count = 0;
+        }
+
+
 
 
     }
--- RobotInterface/Assets/Scripts/Playback/Full_Buttons.cs
+++ RobotInterface/Assets/Scripts/Playback/Full_Buttons.cs
@@ -23,38 +23,55 @@ public class Full_Buttons : MonoBehaviour
         target = GameObject.Find("Target");
         positions = target.GetComponent<Mouse_drag>().positions;
         point_selected = false;
-        DuplicateTargets= new List<GameObject>();
+        DuplicateTargets = new List<GameObject>();
     }
     ///<summary>
     ///Refreshes the numbering on the dropdown menu
     ///</summary>
     private void RefreshDropdownNumbering()
     {
-        for (int i = 0; i < dropdown.options.Count; i++)
+        for (int i = 0; i < dropdown.options.Count; i++)  //Go through each dropdown option
         {
-            dropdown.options[i].text = (i + 1).ToString() + ": " + dropdown.options[i].text.Split(':')[1];
+            dropdown.options[i].text = (i + 1).ToString() + ": " + dropdown.options[i].text.Split(':')[1]; //refresh the numbering on the dropdown
         }
     }
-    private GameObject CreateTargetSphere(Vector3 position, bool render){
-        GameObject newobj = Instantiate(GameObject.Find("DuplicateTarget"));
+
+    ///<summary>
+    ///Creates a duplicate target sphere and renders it only if boolean render enables it
+    ///</summary>
+    private GameObject CreateTargetSphere(Vector3 position, bool render)
+    {
+        GameObject newobj = Instantiate(GameObject.Find("DuplicateTarget")); //Creating a new gameobject
         MeshRenderer[] renderers = newobj.GetComponentsInChildren<MeshRenderer>();
-        foreach(MeshRenderer renderer in renderers){
-           renderer.enabled = render;
-        }
-        newobj.transform.position = position;
-        newobj.transform.localScale = target.transform.localScale;
-        return newobj;
-    }
-    private void TargetPointFunctionRender(bool on){
-        foreach (GameObject newobj in DuplicateTargets)
+        foreach (MeshRenderer renderer in renderers) //go through each Mesh Renderer and set the enabled value to the parameter
         {
-            RenderTarget(newobj,on);
+            renderer.enabled = render;
+        }
+        newobj.transform.position = position; //set the transform position of the newly created object to the position parameter
+        newobj.transform.localScale = target.transform.localScale; //set local scale to correct target scale
+        return newobj; //return new object
+    }
+
+    ///<summary>
+    ///Renders all targets in the duplicate targets array
+    ///</summary>
+    private void TargetPointFunctionRender(bool on)
+    {
+        foreach (GameObject newobj in DuplicateTargets) //go through each gameobject in Duplicate Targets
+        {
+            RenderTarget(newobj, on); //use the render function
         }
     }
-    private void RenderTarget(GameObject obj, bool on){
-        MeshRenderer[] renderers = obj.GetComponentsInChildren<MeshRenderer>();
-        foreach(MeshRenderer renderer in renderers){
-           renderer.enabled = on;
+
+    ///<summary>
+    ///Renders a target
+    ///</summary>
+    private void RenderTarget(GameObject obj, bool on)
+    {
+        MeshRenderer[] renderers = obj.GetComponentsInChildren<MeshRenderer>(); //get the renderers
+        foreach (MeshRenderer renderer in renderers)
+        {
+            renderer.enabled = on; //set the render enabled value to parameter boolean on
         }
     }
     ///<summary>
@@ -62,25 +79,27 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public Vector3 String_to_Vector3(string val)
     {
-        Vector3 retval;
-        string[] values = new string[3];
+        Vector3 retval; //create a vector to return
+        string[] values = new string[3];//create a string array to store values of x y and z in string form
         values = val.Split(',');
+        //set x, y, and z values from string array
         retval.x = float.Parse(values[0]);
         retval.y = float.Parse(values[1]);
         retval.z = float.Parse(values[2]);
 
-        return retval;
+        return retval; // return vector
     }
     ///<summary>
     ///takes in a Vector3 and returns a string in the form: "x,y,z"
     ///</summary>
     public string Vector3_to_String(Vector3 val)
     {
-        string retval;
+        string retval; // create a string
+        //cocatenate x,y,and z strings
         retval = val.x.ToString("F2") + ",";
         retval += val.y.ToString("F2") + ",";
         retval += val.z.ToString("F2");
-        return retval;
+        return retval; // return string
     }
 
     ///<summary>
@@ -88,30 +107,35 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public void Add_Point()
     {
-        Vector3 newVector = target.transform.position;
-        positions.Add(newVector);
-        dropdown.options.Add(new UnityEngine.UI.Dropdown.OptionData(positions.Count.ToString() + ": " + Vector3_to_String(newVector)));
-        dropdown.value = positions.Count - 1;
-        dropdown.RefreshShownValue();
-        DuplicateTargets.Add(CreateTargetSphere(newVector,ShowAllPointsToggle.isOn));
+        Vector3 newVector = target.transform.position; //set a new vector holding the current target position
+        positions.Add(newVector); //using list function to add this new vector 
+        dropdown.options.Add(new UnityEngine.UI.Dropdown.OptionData(positions.Count.ToString() + ": " + Vector3_to_String(newVector))); //add the option to the dropdown menu
+        dropdown.value = positions.Count - 1;//set the dropdown value to change the dropdown shown value
+        dropdown.RefreshShownValue(); //Dropdown refresh shown value
+        DuplicateTargets.Add(CreateTargetSphere(newVector, ShowAllPointsToggle.isOn)); //Add a duplicate target to be shown
     }
     ///<summary>
     ///Deletes point currently selected on the dropdown menu
     ///</summary>
     public void Delete_Point()
     {
+        //first check to see if positions have a point to be deleted
         if (positions.Count >= 1)
         {
+            //remove the specified value
             positions.RemoveRange(dropdown.value, 1);
             dropdown.options.RemoveRange(dropdown.value, 1);
+            //refresh 
             RefreshDropdownNumbering();
             dropdown.RefreshShownValue();
-            RenderTarget(DuplicateTargets[dropdown.value],false);
-            DuplicateTargets.RemoveRange(dropdown.value,1);
+            //render specified target off
+            RenderTarget(DuplicateTargets[dropdown.value], false);
+            DuplicateTargets.RemoveRange(dropdown.value, 1);
 
         }
         else
         {
+            //print to the console 
             print("No points to delete");
         }
     }
@@ -120,9 +144,11 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public void CoordXEdit()
     {
-
+        //get value of the edit field
         float currValue = float.Parse(GameObject.Find("CoordX").GetComponent<UnityEngine.UI.InputField>().text);
+        //create new vector with the new X value
         Vector3 newVector = new Vector3(currValue, positions[dropdown.value].y, positions[dropdown.value].z);
+        //update interface to include the new vector
         dropdown.options[dropdown.value].text = (dropdown.value + 1).ToString() + ": " + Vector3_to_String(newVector);
         positions[dropdown.value] = newVector;
         target.transform.position = newVector;
@@ -132,9 +158,11 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public void CoordYEdit()
     {
-
+        //get value of the edit field
         float currValue = float.Parse(GameObject.Find("CoordY").GetComponent<UnityEngine.UI.InputField>().text);
+        //create new vector with the new X value
         Vector3 newVector = new Vector3(positions[dropdown.value].x, currValue, positions[dropdown.value].z);
+        //update interface to include the new vector
         dropdown.options[dropdown.value].text = (dropdown.value + 1).ToString() + ": " + Vector3_to_String(newVector);
         positions[dropdown.value] = newVector;
         target.transform.position = newVector;
@@ -144,9 +172,11 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public void CoordZEdit()
     {
-
+        //get value of the edit field
         float currValue = float.Parse(GameObject.Find("CoordZ").GetComponent<UnityEngine.UI.InputField>().text);
+        //create new vector with the new X value
         Vector3 newVector = new Vector3(positions[dropdown.value].x, positions[dropdown.value].y, currValue);
+        //update interface to include the new vector
         dropdown.options[dropdown.value].text = (dropdown.value + 1).ToString() + ": " + Vector3_to_String(newVector);
         positions[dropdown.value] = newVector;
         target.transform.position = newVector;
@@ -156,8 +186,11 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public void Change_target_position()
     {
+        //get the vector that the dropdown menu now has
         Vector3 pos = positions[dropdown.value];
+        //set target position to the position
         target.transform.position = pos;
+        //update coordinate edit fields
         GameObject.Find("CoordX").GetComponent<UnityEngine.UI.InputField>().text = pos.x.ToString("F2");
         GameObject.Find("CoordY").GetComponent<UnityEngine.UI.InputField>().text = pos.y.ToString("F2");
         GameObject.Find("CoordZ").GetComponent<UnityEngine.UI.InputField>().text = pos.z.ToString("F2");
@@ -167,21 +200,33 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public void Insert_Point_Before()
     {
-        Vector3 newVector = GameObject.Find("Target").transform.position;
+        //Get position of the target
+        Vector3 newVector = target.transform.position;
+        //First check if a point is selected
         if (point_selected)
         {
+            //set newVector equal to the selected point
             newVector = selected_point;
-            point_selected = false;
-            DuplicateTargets.Insert(dropdown.value,selectedDuplicateTarget);
+            //Insert DuplicateTarget into the correct position
+            DuplicateTargets.Insert(dropdown.value, selectedDuplicateTarget);
         }
-        else{
-            DuplicateTargets.Insert(dropdown.value,CreateTargetSphere(newVector,ShowAllPointsToggle.isOn));
+        else
+        {
+            //Insert a newly created Duplicate target
+            DuplicateTargets.Insert(dropdown.value, CreateTargetSphere(newVector, ShowAllPointsToggle.isOn));
         }
+        //Insert newVector into positions array
         positions.Insert(dropdown.value, newVector);
+        //Update dropdown to include new point
         dropdown.options.Insert(dropdown.value, new UnityEngine.UI.Dropdown.OptionData(":" + Vector3_to_String(newVector)));
-        RenderTarget(DuplicateTargets[dropdown.value],ShowAllPointsToggle.isOn);
+        //Render the newly inserted target if the toggle is on
+        RenderTarget(DuplicateTargets[dropdown.value], ShowAllPointsToggle.isOn);
+        //refresh interface dropdown
         RefreshDropdownNumbering();
         dropdown.RefreshShownValue();
+        //Change point_selected to false since if a point is selected it has now been inserted or if there is no point selected
+        //then it is already false
+        point_selected = false;
 
     }
     ///<summary>
@@ -189,23 +234,35 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public void Insert_Point_After()
     {
+        //Get position of the target
         Vector3 newVector = GameObject.Find("Target").transform.position;
+        //First check if a point is selected
         if (point_selected)
         {
+            //set newVector equal to the selected point
             newVector = selected_point;
-            point_selected = false;
-            DuplicateTargets.Insert(dropdown.value,selectedDuplicateTarget);
+            //Insert DuplicateTarget into the value+1 position to insert after
+            DuplicateTargets.Insert(dropdown.value + 1, selectedDuplicateTarget);
         }
-        else{
-            DuplicateTargets.Insert(dropdown.value,CreateTargetSphere(newVector,ShowAllPointsToggle.isOn));
+        else
+        {
+            DuplicateTargets.Insert(dropdown.value + 1, CreateTargetSphere(newVector, ShowAllPointsToggle.isOn));
         }
+        //Insert newVector into positions array
         positions.Insert(dropdown.value + 1, newVector);
+        //Update dropdown to include new point and Duplicate target to include new point
         dropdown.options.Insert(dropdown.value + 1, new UnityEngine.UI.Dropdown.OptionData(":" + Vector3_to_String(newVector)));
-        DuplicateTargets.Insert(dropdown.value+1,selectedDuplicateTarget);
+        DuplicateTargets.Insert(dropdown.value + 1, selectedDuplicateTarget);
+        //update dropdown value
         dropdown.value = dropdown.value + 1;
-        RenderTarget(DuplicateTargets[dropdown.value+1],ShowAllPointsToggle.isOn);
+        //Render the newly inserted target if the toggle is on
+        RenderTarget(DuplicateTargets[dropdown.value + 1], ShowAllPointsToggle.isOn);
+        //refresh interface dropdown
         RefreshDropdownNumbering();
         dropdown.RefreshShownValue();
+        //Change point_selected to false since if a point is selected it has now been inserted or if there is no point selected
+        //then it is already false
+        point_selected = false;
     }
 
     ///<summary>
@@ -213,20 +270,24 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public void Load()
     {
+        //create a path string that starts at the data path and starts with save.txt
         string path = Application.dataPath + "/Save.txt";
+        //read file into an array of strings with each entry being a different line
         string[] strings_vector = File.ReadAllLines(path);
-        //reset
-
+        //reset interface
+        DuplicateTargets.Clear();
         dropdown.ClearOptions();
         positions.Clear();
         GameObject.Find("CoordX").GetComponent<UnityEngine.UI.InputField>().text = "";
         GameObject.Find("CoordY").GetComponent<UnityEngine.UI.InputField>().text = "";
         GameObject.Find("CoordZ").GetComponent<UnityEngine.UI.InputField>().text = "";
-        //end of reseting
+        //For each string in the string array
         for (int i = 0; i < strings_vector.GetLength(0); i++)
         {
+            //get the vector split from the numbering
             Vector3 newVector;
             string[] newString = strings_vector[i].Split(':');
+            //check if there is a numbering or not
             if (newString.Length == 1)
             {
                 newVector = String_to_Vector3(strings_vector[i]);
@@ -235,11 +296,12 @@ public class Full_Buttons : MonoBehaviour
             {
                 newVector = String_to_Vector3(newString[1]);
             }
+            //add point to the interface
             positions.Add(new Vector3(newVector.x, newVector.y, newVector.z));
             dropdown.options.Add(new UnityEngine.UI.Dropdown.OptionData((i + 1).ToString() + ": " + Vector3_to_String(newVector)));
-            DuplicateTargets.Add(CreateTargetSphere(newVector,ShowAllPointsToggle.isOn));
+            DuplicateTargets.Add(CreateTargetSphere(newVector, ShowAllPointsToggle.isOn));
         }
-
+        //refresh dropdown menu
         dropdown.RefreshShownValue();
     }
     ///<summary>
@@ -247,15 +309,31 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public void Save()
     {
+        //start path as the save.txt in the application's datapath
         string path = Application.dataPath + "/Save.txt";
-        if (!File.Exists(path))
+        //Check if the file path exists
+        if (!File.Exists(path)) //if it doens't create a new file with path
         {
             File.WriteAllText(path, "");
         }
-        else
+        else //if it does find a suitable filename
         {
-            File.Delete(path);
-            File.WriteAllText(path, "");
+            bool PathFound = false;
+            int counter = 1;
+            while (!PathFound) //cycle through file names in the form save(1).txt,save(2).txt
+            {
+                path = Application.dataPath + "/Save" + "(" + counter.ToString() + ")" + ".txt";
+                if (File.Exists(path))
+                {
+                    continue;
+                }
+                else
+                {
+                    File.WriteAllText(path, "");
+                    PathFound = true;
+                }
+                counter++;
+            }
         }
         for (int i = 0; i < dropdown.options.Count; i++)
         {
@@ -268,37 +346,45 @@ public class Full_Buttons : MonoBehaviour
     ///</summary>
     public void SelectPoint()
     {
-        if(!point_selected){
-        point_selected = true;
-        selected_point = positions[dropdown.value];
-        selectedDuplicateTarget = DuplicateTargets[dropdown.value];
-        RenderTarget(DuplicateTargets[dropdown.value],false);
-        dropdown.options.RemoveRange(dropdown.value, 1);
-        DuplicateTargets.RemoveRange(dropdown.value,1);
-        RefreshDropdownNumbering();
-        dropdown.RefreshShownValue();
+        //First check if a point is selected or not
+        if (!point_selected) //If a point is not selected, select the point at the current dropdown menu value
+        {
+            //Set global values for selected point
+            point_selected = true;
+            selected_point = positions[dropdown.value];
+            selectedDuplicateTarget = DuplicateTargets[dropdown.value];
+            // Turn the Duplicate Target sphere off
+            RenderTarget(DuplicateTargets[dropdown.value], false); 
+            dropdown.options.RemoveRange(dropdown.value, 1);
+            DuplicateTargets.RemoveRange(dropdown.value, 1);
+            RefreshDropdownNumbering();
+            dropdown.RefreshShownValue();
         }
-        if(point_selected){
+        if (point_selected) //If a point is selected let the user know a point is already selected
+        {
             print("Point Already Selected:");
             print(selected_point);
         }
-        
+
     }
     ///<summary>
     ///Rotates the camera around the robot by 10 degrees
     ///</summary>
     public void Rotate_Camera()
     {
+        //Rotate the CameraRotater's transform by 10 degrees in the 10 degrees
         GameObject.Find("CameraRotater").transform.Rotate(0, 10, 0);
     }
-    public void ShowPoints(){
-        TargetPointFunctionRender(ShowAllPointsToggle.isOn);
+    public void ShowPoints()
+    {
+        //Render all points if the toggle is On for Showing all Points
+        TargetPointFunctionRender(ShowAllPointsToggle.isOn); 
     }
 
 
-    
 
-    
+
+
 
 
 }
